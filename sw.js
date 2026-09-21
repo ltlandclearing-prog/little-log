6cf371d04dfedb8bc26683a33690261f883f308f
ltlandclearing-prog <ltlandclearing@gmail.com>
2026-09-20 19:19:21 -0500

    Updated files

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'little-log-v43';
+const CACHE = 'little-log-v49';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
