5ae91fc9219adbcef1f7e7f85b22a7e1f97b916f
ltlandclearing-prog <ltlandclearing@gmail.com>
2026-09-26 10:21:42 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'little-log-v58';
+const CACHE = 'little-log-v59';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
